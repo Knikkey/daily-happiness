dee49bee628b24b37134551c4c8b70fe55d6f1a1
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-24 00:29:14 +0900

    modal template

---
+++ src/pages/modals/modalTemplate/ModalTemplate.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren } from "react";
+import styles from "./styles/ModalTemplate.module.css";
+
+interface ModalProps {
+  state?: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+  children: JSX.Element | JSX.Element[];
+}
+
+export default function ModalTemplate({
+  state,
+  setState,
+  children,
+}: ModalProps) {
+  return (
+    <div className={styles.modal}>
+      <div className={styles["modal-relative"]}>
+        <button onClick={() => setState(false)} className={styles["close-btn"]}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
