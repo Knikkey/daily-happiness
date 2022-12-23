5664851aa30d6fb6ed35d784b56e7906ffa510f9
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-23 09:20:33 +0900

    guest signup functionality implemented

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { useLogout } from "../../hooks/useLogout";
+import { auth } from "../../firebase/config";
+
+import Login from "../login/Login";
 
 export default function Dashboard() {
+  const [showSignup, setShowSignup] = useState(false);
+  const guestSignup = true;
+
+  //hooks
+  const { currentUser } = auth;
   const { logout } = useLogout();
+
   return (
     <div>
       <p>Dashboard</p>
+      {currentUser?.isAnonymous && (
+        <>
+          <p>
+            You're currently using a guest account. Please{" "}
+            <button onClick={() => setShowSignup(true)}>Sign up</button> to save
+            your data.
+          </p>
+          {showSignup && <Login guestSignup={guestSignup} />}
+        </>
+      )}
       <button onClick={logout}>Log out</button>
     </div>
   );
