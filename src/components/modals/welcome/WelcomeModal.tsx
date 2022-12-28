c0c7bd0568d10c0e69acb253c15ca2fbcab72ed1
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-28 15:40:16 +0900

    started welcome modal

--- src/components/modals/welcome/WelcomeModal.tsx
+++ src/components/modals/welcome/WelcomeModal.tsx
@@ -1,3 +1,14 @@
 export default function WelcomeModal() {
-  return <div>WelcomeModal</div>;
+  return (
+    <div>
+      <p>Happy days, NAME</p>
+      <p>
+        Click the <span>pencil cloud on the right</span> to{" "}
+        <span>record a happy memory</span> or click the{" "}
+        <span>book cloud on the right</span> to{" "}
+        <span>view some that you've already recorded</span>.
+      </p>
+      <p>Have a wonderful day :)</p>
+    </div>
+  );
 }
