c16193cc43f32ea9abb4dbe7cac80ded5011eff2
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-28 19:17:50 +0900

    added background music

--- src/components/modals/welcome/WelcomeModal.tsx
+++ src/components/modals/welcome/WelcomeModal.tsx
@@ -1,14 +1,49 @@
+import { useState } from "react";
+import { useAuthContext } from "../../../hooks/useAuthContext";
+import { auth } from "../../../firebase/config";
+import ModalTemplate from "../modalTemplate/ModalTemplate";
+import leftCloud from "../../../pages/dashboard/left-cloud.png";
+import rightCloud from "../../../pages/dashboard/right-cloud.png";
+
+import styles from "./styles/WelcomeModal.module.css";
+
 export default function WelcomeModal() {
+  const [showWelcome, setShowWelcome] = useState(true);
+  const { user } = useAuthContext();
+  const { currentUser } = auth;
+
   return (
-    <div>
-      <p>Happy days, NAME</p>
-      <p>
-        Click the <span>pencil cloud on the right</span> to{" "}
-        <span>record a happy memory</span> or click the{" "}
-        <span>book cloud on the right</span> to{" "}
-        <span>view some that you've already recorded</span>.
-      </p>
-      <p>Have a wonderful day :)</p>
-    </div>
+    <>
+      {showWelcome && (
+        <ModalTemplate setProp={setShowWelcome}>
+          <div className={styles["welcome-container"]}>
+            <p className={styles.welcome}>
+              Happy days,{" "}
+              {currentUser?.isAnonymous ? user.displayName : "friend"}
+            </p>
+            <p className={styles.message}>
+              Click the{" "}
+              <img
+                src={leftCloud}
+                className={styles.left}
+                alt="left cloud with pencil"
+              />{" "}
+              to <span className={styles.record}>record a happy memory</span> or
+              click the{" "}
+              <img
+                src={rightCloud}
+                className={styles.right}
+                alt="right cloud with book"
+              />{" "}
+              <span className={styles.read}>
+                view some that you've already recorded
+              </span>
+              .
+            </p>
+            <p className={styles.end}>Have a wonderful day :)</p>
+          </div>
+        </ModalTemplate>
+      )}
+    </>
   );
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,25 +1,41 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useLogout } from "../../hooks/useLogout";
 import { auth } from "../../firebase/config";
 import leftCloud from "./left-cloud.png";
 import rightCloud from "./right-cloud.png";
 import avatar from "./avatar.png";
+import happySong from "./happySong.mp3";
 
 import Login from "../login/Login";
 
 import styles from "./styles/Dashboard.module.css";
 import InputModal from "../../components/modals/input/InputModal";
 import Memories from "../../components/modals/memories/Memories";
+import WelcomeModal from "../../components/modals/welcome/WelcomeModal";
 
 export default function Dashboard() {
   const [showSignup, setShowSignup] = useState(false);
   const [showInput, setShowInput] = useState(false);
   const [showMemories, setShowMemories] = useState(false);
+  const [mute, setMute] = useState(false);
   const guestSignup = true;
+  const audio = useRef<HTMLAudioElement | null>(null);
 
   //hooks
   const { currentUser } = auth;
   const { logout } = useLogout();
+
+  //functions
+  const handleMusic = () => {
+    setMute((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (audio.current) {
+      audio.current.volume = 0.5;
+      !mute ? audio.current?.play() : audio.current.pause();
+    }
+  }, [mute]);
 
   return (
     <div className={styles["dashboard-container"]}>
@@ -56,6 +72,8 @@ export default function Dashboard() {
           )}
         </>
       )}
+      {/* ****************WELCOME MODAL *****************/}
+      <WelcomeModal />
       {/* ****************AVATAR *****************/}
       <div className={styles["avatar-container"]}>
         <img
@@ -79,6 +97,41 @@ export default function Dashboard() {
       {/* ****************LOGOUT *****************/}
       <button className={styles["logout-btn"]} onClick={logout}>
         Log out
+      </button>
+      {/* ****************AUDIO *****************/}
+      <audio ref={audio} src={happySong} autoPlay loop />
+
+      <button className={styles["sound-btn"]} onClick={handleMusic}>
+        {!mute && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        )}
+        {mute && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        )}
       </button>
     </div>
   );
