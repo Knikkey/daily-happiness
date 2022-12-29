106ab8eb921d827ee0db0af3607855c94918f562
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-29 12:42:19 +0900

    done... I think? For now...

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -39,105 +39,105 @@ export default function Login({ guestSignup }: Prop) {
   };
 
   return (
-    // <div className={styles.centered}>
-    <div className={styles["login-container"]}>
-      <p className={styles.subtitle}>
-        {signupPage || guestSignup ? "Sign up" : "Login"}
-      </p>
+    <div className={styles.centered}>
+      <div className={styles["login-container"]}>
+        <p className={styles.subtitle}>
+          {signupPage || guestSignup ? "Sign up" : "Login"}
+        </p>
 
-      <form onSubmit={submitHandler}>
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.target.value)
-          }
-        />
-        {signupPage && (
-          <>
-            <label htmlFor="displayName">Display Name:</label>
-            <input
-              id="displayName"
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setDisplayName(e.target.value)
-              }
-            />
-          </>
-        )}
-        {guestSignup && (
-          <>
-            <label htmlFor="displayName">Display Name:</label>
-            <input
-              id="displayName"
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setDisplayName(e.target.value)
-              }
-            />
-          </>
-        )}
-        <label htmlFor="password">Password:</label>
-        <input
-          id="password"
-          type={showPassword ? "text" : "password"}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        />
-        <div className={styles.checkbox}>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="email">Email:</label>
           <input
-            id="passwordCheckBox"
-            type="checkbox"
-            onClick={() => setShowPassword((prev) => !prev)}
+            id="email"
+            type="email"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
           />
-          <label htmlFor="passwordCheckBox">Show password</label>
+          {signupPage && (
+            <>
+              <label htmlFor="displayName">Display Name:</label>
+              <input
+                id="displayName"
+                type="text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDisplayName(e.target.value)
+                }
+              />
+            </>
+          )}
+          {guestSignup && (
+            <>
+              <label htmlFor="displayName">Display Name:</label>
+              <input
+                id="displayName"
+                type="text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDisplayName(e.target.value)
+                }
+              />
+            </>
+          )}
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            type={showPassword ? "text" : "password"}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+          />
+          <div className={styles.checkbox}>
+            <input
+              id="passwordCheckBox"
+              type="checkbox"
+              onClick={() => setShowPassword((prev) => !prev)}
+            />
+            <label htmlFor="passwordCheckBox">Show password</label>
+          </div>
+
+          {/* **********************LOGIN BUTTONS********************** */}
+          {signupPage && (
+            <button type="submit">
+              {isPending ? "Authenticating..." : "Sign up"}
+            </button>
+          )}
+          {guestSignup && (
+            <button type="submit">
+              {isPending ? "Authenticating..." : "Sign up"}
+            </button>
+          )}
+          {!signupPage && !guestSignup && (
+            <button type="submit">
+              {isPending ? "Authenticating..." : "Log in"}
+            </button>
+          )}
+          {!guestSignup && (
+            <button onClick={() => setGuest(true)} className={styles.guest}>
+              Log in as a guest
+            </button>
+          )}
+
+          {/* **********************ERROR MESSAGES********************** */}
+          {signupError && <p className={styles.error}>{signupError}</p>}
+          {loginError && <p className={styles.error}>{loginError}</p>}
+        </form>
+
+        {/* **********************BLUE BUTTONS********************** */}
+        <div className={styles["signup-container"]}>
+          {signupPage && !guestSignup && (
+            <button onClick={() => setSignupPage(false)}>
+              Return to login page
+            </button>
+          )}
+          {!signupPage && !guestSignup && (
+            <>
+              <button onClick={() => setSignupPage(true)}>Sign up</button>
+              <button>Forgot username</button>
+              <button>Forgot password</button>
+            </>
+          )}
         </div>
-
-        {/* **********************LOGIN BUTTONS********************** */}
-        {signupPage && (
-          <button type="submit">
-            {isPending ? "Authenticating..." : "Sign up"}
-          </button>
-        )}
-        {guestSignup && (
-          <button type="submit">
-            {isPending ? "Authenticating..." : "Sign up"}
-          </button>
-        )}
-        {!signupPage && !guestSignup && (
-          <button type="submit">
-            {isPending ? "Authenticating..." : "Log in"}
-          </button>
-        )}
-        {!guestSignup && (
-          <button onClick={() => setGuest(true)} className={styles.guest}>
-            Log in as a guest
-          </button>
-        )}
-
-        {/* **********************ERROR MESSAGES********************** */}
-        {signupError && <p className={styles.error}>{signupError}</p>}
-        {loginError && <p className={styles.error}>{loginError}</p>}
-      </form>
-
-      {/* **********************BLUE BUTTONS********************** */}
-      <div className={styles["signup-container"]}>
-        {signupPage && !guestSignup && (
-          <button onClick={() => setSignupPage(false)}>
-            Return to login page
-          </button>
-        )}
-        {!signupPage && !guestSignup && (
-          <>
-            <button onClick={() => setSignupPage(true)}>Sign up</button>
-            <button>Forgot username</button>
-            <button>Forgot password</button>
-          </>
-        )}
       </div>
     </div>
-    // </div>
   );
 }
