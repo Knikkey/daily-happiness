46362bb80d631f1798c08e6db73c2b1df3c70598
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-01-03 09:10:11 +0900

    cleaned up css files

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useAuthContext } from "./hooks/useAuthContext";
 
 import Home from "./pages/Home";
 
-import "./App.css";
-
 function App() {
   const { authIsReady } = useAuthContext();
   return <div className="App">{authIsReady && <Home />}</div>;
